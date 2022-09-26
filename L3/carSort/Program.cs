d3ba63b1f5a53ba845ee2c1fe029e61b1468f006
razwaz <andpdud@gmail.com>
2022-09-26 11:10:14 +0200

    update

--- L3/carSort/Program.cs
+++ L3/carSort/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using carSort;
 
-Console.WriteLine("Hello, World!");
+namespace carSort{
+    class Program {
+        static void Main(){
+            List<Car> cars = new List<Car>(){
+                new Car("Skoda", "Fabia", 50000m),
+                new Car("Skoda", "Octavia", 60000m),
+                new Car("Fiat", "500", 12345m),
+                new Car("Ford", "Mustang", 9000000m),
+                new Car("Ford", "Mustang", 9000001m)
+            };
+            IComparer<Car> cc = null;
+
+            while(true){
+                string s = Console.ReadLine();
+                if(s == "q"){
+                    return;
+                }
+                if(s == "m"){
+                    cc = new MakeComparer();
+                }
+                if (s == "p"){
+                    cc = new PriceComparer();
+                }
+                cars.Sort(cc);
+                Console.WriteLine("Sorted by " + cc.GetType().Name);
+                foreach (Car car in cars){
+                Console.WriteLine($" {car.Make} {car.Model} {car.Price}");
+            }
+            }
+
+        
+
+        }
+    }
+}
---
+++ L3/carSort/sort.cs
@@ -0,0 +1,63 @@
+using System;
+
+namespace carSort{
+
+    class Car : IComparable<Car>{
+        public string Make{get; private set;}
+        public string Model{get; private set;}
+        public decimal Price{get; private set;}
+
+        public Car(string make, string model, decimal price){
+            Make = make;
+            Model = model;
+            Price = price;
+        }
+        /*public int CompareTo(object obj){
+            return Price.compareTo(((Car)obj).Price);
+        }*/
+
+        public int CompareTo(Car other)
+        {
+            return Price.CompareTo(other.Price);
+        }
+    }
+      
+    class CarComparer: IComparer<Car>{
+        
+        public int Compare(Car? x, Car? y){
+
+            int makeOrder = x.Make.CompareTo(y.Make);
+            if (makeOrder != 0) {
+                return makeOrder;
+            } 
+            int modelOrder = x.Model.CompareTo(y.Model);
+            if (modelOrder != 0) {
+                return modelOrder;
+            }
+            return x.Price.CompareTo(y.Price);
+        }
+
+    }
+
+    class MakeComparer: IComparer<Car>{
+        
+        public int Compare(Car? x, Car? y){
+
+        return x.Make.CompareTo(y.Make);
+
+        }
+    }
+
+    class ModelComparer : IComparer<Car>{
+        public int Compare(Car? x, Car? y){
+            return x.Model.CompareTo(y.Model);
+        }
+    }
+
+    class PriceComparer : IComparer<Car>{
+        public int Compare(Car? x, Car? y){
+            return x.Price.CompareTo(y.Price);
+        }
+    }
+
+}
