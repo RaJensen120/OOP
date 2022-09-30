f70b38b8b6c810ef678f6e0417de27ecd8513cfe
razwaz <andpdud@gmail.com>
2022-09-30 10:15:28 +0200

    l4 menu semi functional

--- L4/Menu.cs
+++ L4/Menu.cs
@@ -9,13 +9,53 @@ public class Menu
     
     public string Title { get; set; }
 
+    public List<MenuItem> MenuItems = new List<MenuItem>();
+
     public void Add(MenuItem item)
     {
-        
+        MenuItems.Add(item);
     }
 
     public void Start()
     {
-        
+        bool done = false;
+        int selected = 0;
+        while (!done)
+        {
+            for (int i = 0; i < MenuItems.Count; i++)
+            {
+                if (selected == i)
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("> ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+
+                Console.WriteLine(MenuItems[i].Title);
+
+                Console.ResetColor();
+            }
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.UpArrow:
+                    selected = Math.Max(0, selected - 1);
+                    break;
+                case ConsoleKey.DownArrow:
+                    selected = Math.Min(MenuItems.Count - 1, selected + 1);
+                    break;
+                case ConsoleKey.Enter:
+                    done = true;
+                    break;
+            }
+
+            if (!done)
+                Console.CursorTop = Console.CursorTop - MenuItems.Count;
+        }
+
+        Console.WriteLine($"Selected {selected}.");
     }
 }
--- L4/Program.cs
+++ L4/Program.cs
@@ -11,46 +11,93 @@ namespace L4
             Menu menu = new Menu("fancymenu");
 
             menu.Add(
-            new MenuItem(
-                "Punkt1",
-                "Indhold af punkt 1... det er indtil videre bare tekst"));
-
-            menu.Add(
-            new MenuItem(
-                "Punkt2 ",
-                "Indhold af punkt 2... det er indtil videre også bare tekst"));
+                new MenuItem(
+                    "Punkt1",
+                    "Indhold af punkt 1... det er indtil videre bare tekst"));
 
             menu.Add(
                 new MenuItem(
-                "Et lidt læ ngere menupunkt ",
-                "Indhold af punkt 3... det er indtil videre også bare tekst"));
+                    "Punkt2 ",
+                    "Indhold af punkt 2... det er indtil videre også bare tekst"));
 
+            menu.Add(
+                new MenuItem(
+                    "Et lidt læ ngere menupunkt ",
+                    "Indhold af punkt 3... det er indtil videre også bare tekst"));
+            
             menu.Start();
-        }
 
+            /*
+            int optionsCount = 5;
 
+            int selected = 0;
 
+            bool done = false;
 
-
-
-
-
-
-
-
-        /*
-        static void Main(string[] args)
-        {
-            string url = "http://www.dr.dk/nyheder/service/feeds/allenyheder";
-            XmlReader reader = XmlReader.Create(url);
-            SyndicationFeed feed = SyndicationFeed.Load(reader);
-            reader.Close();
-            foreach (SyndicationItem item in feed.Items)
+            while (!done)
             {
-                Console.WriteLine(item.Title.Text);
-                //Console.WriteLine(item.Summary.Text);
+                for (int i = 0; i < optionsCount; i++)
+                {
+                    if (selected == i)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.Write("> ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+
+                    Console.WriteLine(i);
+
+                    Console.ResetColor();
+                }
+
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selected = Math.Max(0, selected - 1);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selected = Math.Min(optionsCount - 1, selected + 1);
+                        break;
+                    case ConsoleKey.Enter:
+                        done = true;
+                        break;
+                }
+
+                if (!done)
+                    Console.CursorTop = Console.CursorTop - optionsCount;
             }
+
+            Console.WriteLine($"Selected {selected}.");
         }
-    }*/
+        */
+
+
+
+
+
+
+
+
+
+
+
+            /*
+            static void Main(string[] args)
+            {
+                string url = "http://www.dr.dk/nyheder/service/feeds/allenyheder";
+                XmlReader reader = XmlReader.Create(url);
+                SyndicationFeed feed = SyndicationFeed.Load(reader);
+                reader.Close();
+                foreach (SyndicationItem item in feed.Items)
+                {
+                    Console.WriteLine(item.Title.Text);
+                    //Console.WriteLine(item.Summary.Text);
+                }
+            }
+        }*/
+        }
     }
 }
